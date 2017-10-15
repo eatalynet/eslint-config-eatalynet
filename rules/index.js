120312add5d02024d7f9b18edfd1887bd2791ff3
Manuel Palma <mpalma.job@gmail.com>
2017-10-15 23:18:57 +0200

    Update index.js

--- rules/index.js
+++ rules/index.js
@@ -1,1 +1,17 @@
-
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    'ecmaFeatures': {
+      'jsx': true,
+      'impliedStrict': true,
+      'experimentalObjectRestSpread': true
+    }
+  }
+}
