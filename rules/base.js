8c5c357e2b6ebf211cb4a090f7b11619d903dd3e
Manuel Palma <mpalma.job@gmail.com>
2017-10-15 23:25:27 +0200

    Update base.js

--- rules/base.js
+++ rules/base.js
@@ -1,6 +1,21 @@
 module.exports = {
   rules: {
     /* VARIABLES */
+    
+    // disallow deletion of variables
+    'no-delete-var': 'error',
+    
+    // disallow use of undeclared variables unless mentioned in a /*global */ block
+    'no-undef': 'error',
+
+    // disallow use of undefined when initializing variables
+    'no-undef-init': 'error',
+
+    // disallow use of undefined variable
+    // http://eslint.org/docs/rules/no-undefined
+    // TODO: enable?
+    'no-undefined': 'off',
+    
     // disallow declaration of variables that are not used in the code
     'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
     
