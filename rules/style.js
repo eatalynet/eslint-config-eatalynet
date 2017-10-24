084a187cf3fd58ef0429e9d78eec1f4b9216c976
Manuel Palma <mpalma.job@gmail.com>
2017-10-24 19:54:22 +0200

    Update style.js

--- rules/style.js
+++ rules/style.js
@@ -29,7 +29,7 @@ module.exports = {
                 objects: "always-multiline",
                 imports: "always-multiline",
                 exports: "always-multiline",
-                functions: "always-multiline"
+                functions: "never"
             }
         ],
 
